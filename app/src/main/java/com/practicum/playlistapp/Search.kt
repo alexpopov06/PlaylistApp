3a427e486f6193b710abb2bfa00cef0e21e600bf
Alex <alexpopov06@mail.ru>
2025-04-06 15:23:45 +0300

    конец спринта 9

--- app/src/main/java/com/practicum/playlistapp/Search.kt
+++ app/src/main/java/com/practicum/playlistapp/Search.kt
@@ -1,15 +1,66 @@
 package com.practicum.playlistapp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Search : AppCompatActivity() {
+    private var searchText: String = ""
+    private var inputEditText: EditText? = null
+    companion object {
+        private const val SEARCH_TEXT_KEY = "search_text_key"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val inputEditText = findViewById<EditText>(R.id.inputText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+            inputEditText.setText(searchText)
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                searchText = s?.toString() ?: ""
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+        inputEditText?.setText(searchText)
+    }
+
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/practicum/playlistapp/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistapp/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +17,32 @@ class SettingsActivity : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+        val shareButton = findViewById<Button>(R.id.share)
+        shareButton.setOnClickListener {
+            val share = Intent(Intent.ACTION_SENDTO)
+            val sms = R.string.LinkAndroid
+            share.data = Uri.parse("smsto:")
+            share.putExtra(Intent.EXTRA_TEXT, "sms")
+            startActivity(share)
+        }
+        val writeButton = findViewById<Button>(R.id.writeSup)
+        writeButton.setOnClickListener {
+            val write = Intent(Intent.ACTION_SENDTO)
+            write.data = Uri.parse("mailto:")
+            write.putExtra(Intent.EXTRA_EMAIL, arrayOf("Alexpopov06@mail.ru"))
+            write.putExtra(Intent.EXTRA_SUBJECT, R.string.MessageToDev)
+            write.putExtra(Intent.EXTRA_TEXT, R.string.ThanksToDev)
+            startActivity(write)
+        }
+        val agreeButton = findViewById<Button>(R.id.agreementButton)
+        agreeButton.setOnClickListener {
+            val browse = Intent(Intent.ACTION_VIEW)
+            browse.data = Uri.parse(getString(R.string.AndoidOffer))
+            startActivity(browse)
+        }
+
+
+
 
     }
 }
