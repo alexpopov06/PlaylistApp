519bcb6354efc7224d6779ae24d956a6df122d22
aleksandrpopov <alexpopov06@mail.ru>
2025-08-06 19:33:56 +0300

    ффф

--- app/src/main/java/com/practicum/playlistapp/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistapp/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
-
+фффф
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
