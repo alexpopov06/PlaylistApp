58473b813e07296589ebb388cf3c1c08d63d7e34
aleksandrpopov <alexpopov06@mail.ru>
2025-10-31 20:47:27 +0300

    19 Single Activity, JetPackNav,BottomNav (вторая попытка)

--- app/src/main/java/com/practicum/playlistapp/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistapp/RootActivity.kt
@@ -1,30 +1,53 @@
 package com.practicum.playlistapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.commit
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.practicum.playlistapp.databinding.ActivityRootBinding
-import com.practicum.playlistapp.search.ui.SearchFragment
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.setupWithNavController(navController)
 
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.playerFragment -> hideBottomNavigation()
+                else -> showBottomNavigation()
+            }
+        }
+    }
+
+    fun hideBottomNavigation() {
+        binding.bottomNavigationView.visibility = View.GONE
+        binding.divider.visibility = View.GONE
+
+
+        val params = binding.rootFragmentContainerView.layoutParams as ConstraintLayout.LayoutParams
+        params.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+        binding.rootFragmentContainerView.layoutParams = params
+    }
+
+    fun showBottomNavigation() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+        binding.divider.visibility = View.VISIBLE
+
+        // Восстанавливаем оригинальные констрейнты
+        val params = binding.rootFragmentContainerView.layoutParams as ConstraintLayout.LayoutParams
+        params.bottomToTop = R.id.divider
+        binding.rootFragmentContainerView.layoutParams = params
     }
 }
