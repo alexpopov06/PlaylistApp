f1ac2fad343a497ce52e5a182de0ff2b62aa5df8
aleksandrpopov <alexpopov06@mail.ru>
2025-11-16 12:10:06 +0300

    20 end corutines+ flow 2

--- app/src/main/java/com/practicum/playlistapp/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistapp/RootActivity.kt
@@ -2,52 +2,67 @@ package com.practicum.playlistapp
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.practicum.playlistapp.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityRootBinding
+    private var isKeyboardVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
-        val bottomNavigationView = binding.bottomNavigationView
-        bottomNavigationView.setupWithNavController(navController)
+        setupKeyboardListener()
+        setupNavController()
+    }
 
+    private fun setupNavController() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView)
+                as NavHostFragment
+
+        val navController = navHost.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.playerFragment -> hideBottomNavigation()
-                else -> showBottomNavigation()
-            }
+            if (destination.id == R.id.playerFragment) hideBottomNav()
+            else if (!isKeyboardVisible) showBottomNav()
         }
     }
 
-    fun hideBottomNavigation() {
-        binding.bottomNavigationView.visibility = View.GONE
-        binding.divider.visibility = View.GONE
+    private fun setupKeyboardListener() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
 
+            val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
 
-        val params = binding.rootFragmentContainerView.layoutParams as ConstraintLayout.LayoutParams
-        params.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
-        binding.rootFragmentContainerView.layoutParams = params
+            val keyboardNowVisible = imeHeight > 0
+
+            if (keyboardNowVisible != isKeyboardVisible) {
+                isKeyboardVisible = keyboardNowVisible
+                if (keyboardNowVisible) hideBottomNav()
+                else showBottomNav()
+            }
+
+            insets
+        }
     }
 
-    fun showBottomNavigation() {
+    private fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = View.GONE
+        binding.divider.visibility = View.GONE
+    }
+
+    private fun showBottomNav() {
         binding.bottomNavigationView.visibility = View.VISIBLE
         binding.divider.visibility = View.VISIBLE
-
-        // Восстанавливаем оригинальные констрейнты
-        val params = binding.rootFragmentContainerView.layoutParams as ConstraintLayout.LayoutParams
-        params.bottomToTop = R.id.divider
-        binding.rootFragmentContainerView.layoutParams = params
     }
 }
